4cc48662edbef02c4183e05d3b64e9138043535b
akash <akashshere19@gmail.com>
2023-06-21 16:55:04 +0530

    changes in view file

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('chatbot/',include('chatbotApp.urls'))
+    path('',include('chatbotApp.urls'))
 ]
